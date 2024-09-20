8cc9a191fc12d3fa6f678171f9fee218c0977360
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-09-20 09:40:34 +0200

    All tables completed

--- dpgSQL.sql
+++ dpgSQL.sql
@@ -5,8 +5,9 @@ CREATE TABLE teacher (
     teacher_id_number CHAR(11) PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    phone_number VARCHAR(30) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    phone_number CHAR(10) NOT NULL UNIQUE,
+    email VARCHAR(45) NOT NULL UNIQUE,
+    towm VARCHAR(30),
     office_room_number INT NOT NULL -- could be in the same room, but sharing, so cant be unique
 );
 
@@ -23,65 +24,67 @@ CREATE TABLE class (
 
 
 CREATE TABLE parent (
-    parent_id INT PRIMARY KEY IDENTITY,
+    parent_id_number CHAR(11) PRIMARY KEY ,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    phone_number VARCHAR(30) UNIQUE,
-    email VARCHAR(30) UNIQUE,
-    home_address VARCHAR(30)
+    phone_number CHAR(10) NOT NULL UNIQUE,
+    email VARCHAR(45) NOT NULL UNIQUE,
+    town VARCHAR(30)
 );
 
+ 
 
 CREATE TABLE child (
     child_id INT PRIMARY KEY IDENTITY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     date_of_birth DATE,
+    emergency_contact_number CHAR(10) NOT NULL UNIQUE, 
+    emergency_contact_name VARCHAR(30),
     class_name VARCHAR(30),
-    parent_id INT,
-    FOREIGN KEY (class_name) REFERENCES class(class_name),
-    FOREIGN KEY (parent_id) REFERENCES parent(parent_id)
+    FOREIGN KEY (class_name) REFERENCES class(class_name)
+);
+
+
+CREATE TABLE parent_child (
+    child_id INT NOT NULL,
+    parent_id_number CHAR(11 ) NOT NULL,
+    PRIMARY KEY (child_id, parent_id_number),
+    FOREIGN KEY (child_id) REFERENCES child(child_id),
+    FOREIGN KEY (parent_id_number) REFERENCES parent(parent_id_number)
 );
 
 
 CREATE TABLE qrcode (
     qrcode_id INT PRIMARY KEY IDENTITY,
-    url VARCHAR(255),
-    time_generated DATETIME,
-    parent_id INT,
-    child_id INT,
-    date DATE, 
-    FOREIGN KEY (parent_id) REFERENCES parent(parent_id),
+    qr_code_url VARCHAR(255) NOT NULL UNIQUE, --qr code data usually this long
+    drop_off_time TIME NOT NULL, -- drop off time same as the time the qr code will be generated
+    drop_off_date DATE NOT NULL, 
+    parent_id_number CHAR(11) NOT NULL,
+    child_id INT, 
+    FOREIGN KEY (parent_id_number) REFERENCES parent(parent_id_number),
     FOREIGN KEY (child_id) REFERENCES child(child_id),
-    UNIQUE (parent_id, child_id, date) 
+    UNIQUE (parent_id_number, child_id, date) 
 );
 
-
+-- qr code is used for only one pickup, ie child can only be pickued up once with that qr code
 CREATE TABLE pickup (
     pickup_id INT PRIMARY KEY IDENTITY,
-    pickup_time TIME,
-    pickup_date DATE,
-    qrcode_id INT,
+    pickup_time TIME NOT NULL,
+    pickup_date DATE NOT NULL,
+    qrcode_id INT NOT NULL,
     FOREIGN KEY (qrcode_id) REFERENCES qrcode(qrcode_id)
 );
 
-
-CREATE TABLE dropOff (
-    dropoff_id INT PRIMARY KEY IDENTITY,
-    dropoff_time TIME,
-    dropoff_date DATE,
-    qrcode_id INT,
-    FOREIGN KEY (qrcode_id) REFERENCES qrcode(qrcode_id)
-);
 
 
 CREATE TABLE audit_log (
     log_id INT PRIMARY KEY IDENTITY,
-    table_name VARCHAR(255) NOT NULL,
-    action VARCHAR(50) NOT NULL,
+    action VARCHAR(30) NOT NULL,
     record_id INT NOT NULL,
     old_values VARCHAR(MAX), 
     new_values VARCHAR(MAX), 
     timestamp DATETIME,
-    performed_by VARCHAR(30)
+    performed_by VARCHAR(30),
+    table_name VARCHAR(255) NOT NULL
 );
