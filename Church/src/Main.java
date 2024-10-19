42cf88fcce40034228c0e077e4ad70ec25c493ea
raplowytee <162456810+raplowytee@users.noreply.github.com>
2024-10-19 09:23:15 +0200

    Add files via upload

---
+++ Church/src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Linus
+ */
+public class Main {
+    public static void main(String[] args) {
+        HomePage hp = new HomePage();
+        hp.show();
+    }
+}
