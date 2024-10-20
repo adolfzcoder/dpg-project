4cdd6d8760ae3908418022a1ca816dda7da67381
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-10-20 11:47:11 +0200

    Edited the add class, not errors

--- Middle Tier/Procedures.sql
+++ Middle Tier/Procedures.sql
@@ -1,5 +1,6 @@
 -- these procedures can be used to quickly view the stored data instead of using longer select statement
-USE childChurchQrSystem
+CREATE DATABASE qrSystemDPG
+USE qrSystemDPG
 CREATE PROC viewAdmin
 AS
 BEGIN
@@ -685,19 +686,18 @@ BEGIN
 END;
 
 
-
 CREATE PROCEDURE spAddClass
-    @className VARCHAR(30),
-    @startTime TIME,
+    @class_name VARCHAR(30),
+    @start_time TIME,
     @venue VARCHAR(30),
-    @hasProjector BIT,
-    @endTime TIME,
-    @ageRangeStart INT,
-    @ageRangeEnd INT
+    @has_projector BIT,
+    @end_time TIME,
+    @age_range_start INT,
+    @age_range_end INT
 AS
 BEGIN
     -- Validate class name for special characters
-    IF @className LIKE '%[^a-zA-Z0-9 ]%'
+    IF @class_name LIKE '%[^a-zA-Z0-9 ]%'
     BEGIN
         RAISERROR('Class name contains special characters.', 16, 1);
         RETURN;
@@ -713,8 +713,8 @@ BEGIN
         BEGIN TRANSACTION;
 
         -- Insert class details
-        INSERT INTO class (class_name, start_time, venue, has_projector, end_time,  age_range_start, age_range_end)
-        VALUES (@className, @startTime, @venue, @hasProjector, @endTime, @ageRangeStart, @ageRangeEnd);
+        INSERT INTO class (class_name, start_time, venue, has_projector, end_time, teacher_id_number, age_range_start, age_range_end)
+        VALUES (@class_name, @start_time, @venue, @has_projector, @endTime, @teacherIdNumber, @ageRangeStart, @ageRangeEnd);
 
         COMMIT TRANSACTION;
     END TRY
@@ -727,4 +727,14 @@ BEGIN
     END CATCH
 END;
 
+DECLARE @class_name VARCHAR(30) = 'Math101';
+DECLARE @start_time TIME = '08:00';
+DECLARE @venue VARCHAR(30) = 'Room 101';
+DECLARE @has_projector BIT = 1;
+DECLARE @end_time TIME = '10:00';
+DECLARE @teacher_id_number CHAR(11) = '12345678901';
+DECLARE @age_range_start INT = 10;
+DECLARE @age_range_end INT = 12;
+
+EXEC spAddClass @class_name, @start_time, @venue, @has_projector, @end_time, @teacher_id_number, @age_range_start, @age_range_end;
 
--- Middle Tier/spAddClasss.sql
+++ Middle Tier/spAddClasss.sql
@@ -1,16 +1,15 @@
 CREATE PROCEDURE spAddClass
-    @className VARCHAR(30),
-    @startTime TIME,
+    @class_name VARCHAR(30),
+    @start_time TIME,
     @venue VARCHAR(30),
-    @hasProjector BIT,
-    @endTime TIME,
-    @teacherIdNumber CHAR(11),
-    @ageRangeStart INT,
-    @ageRangeEnd INT
+    @has_projector BIT,
+    @end_time TIME,
+    @age_range_start INT,
+    @age_range_end INT
 AS
 BEGIN
     -- Validate class name for special characters
-    IF @className LIKE '%[^a-zA-Z0-9 ]%'
+    IF @class_name LIKE '%[^a-zA-Z0-9 ]%'
     BEGIN
         RAISERROR('Class name contains special characters.', 16, 1);
         RETURN;
@@ -27,7 +26,7 @@ BEGIN
 
         -- Insert class details
         INSERT INTO class (class_name, start_time, venue, has_projector, end_time, teacher_id_number, age_range_start, age_range_end)
-        VALUES (@className, @startTime, @venue, @hasProjector, @endTime, @teacherIdNumber, @ageRangeStart, @ageRangeEnd);
+        VALUES (@class_name, @start_time, @venue, @has_projector, @endTime, @teacherIdNumber, @ageRangeStart, @ageRangeEnd);
 
         COMMIT TRANSACTION;
     END TRY
@@ -39,3 +38,14 @@ BEGIN
         EXEC spHandleError;
     END CATCH
 END;
+
+DECLARE @class_name VARCHAR(30) = 'Math101';
+DECLARE @start_time TIME = '08:00';
+DECLARE @venue VARCHAR(30) = 'Room 101';
+DECLARE @has_projector BIT = 1;
+DECLARE @end_time TIME = '10:00';
+DECLARE @teacher_id_number CHAR(11) = '12345678901';
+DECLARE @age_range_start INT = 10;
+DECLARE @age_range_end INT = 12;
+
+EXEC spAddClass @class_name, @start_time, @venue, @has_projector, @end_time, @teacher_id_number, @age_range_start, @age_range_end;
